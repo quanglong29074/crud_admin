e03c3511489c96d5fe2290c8adca06f601f1111b
dungdang <dungprohn1409@gmail.com>
2024-06-22 08:50:01 +0700

    update kết nối db

--- src/main/java/com/example/crud_admin/entity/ClassRoom.java
+++ src/main/java/com/example/crud_admin/entity/ClassRoom.java
@@ -1,7 +1,14 @@
 package com.example.crud_admin.entity;
 
 import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
 @Entity
 @Table(name = "class_room")
 public class ClassRoom {
